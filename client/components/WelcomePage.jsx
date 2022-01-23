54594da9fb926091395d09d6eff21db1a6aafb77
Anna Litovskaya <Litovskaya09@gmail.com>
2022-01-23 14:49:27 -0500

    welcome page styled

--- client/components/WelcomePage.jsx
+++ client/components/WelcomePage.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 function WelcomePage() {
   return (
-    <section className="app-screen" id="welcome-page-text">
-      Hello there! My name is Anna Litovskaya. I am an aspiring Fullstack Web
-      Developer.
+    <section className="app-screen" id="welcome-page-container">
+      <div id="welcome-page-text">
+        <span>Hello there!</span>
+        <span>My name is Anna Litovskaya.</span>
+        <span>I am an aspiring Fullstack Web Developer.</span>
+      </div>
     </section>
   );
 }
