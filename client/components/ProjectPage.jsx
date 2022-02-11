06bdfc4bc846b67f9079784cd927e070d505c130
Anna Litovskaya <Litovskaya09@gmail.com>
2022-02-11 04:46:45 -0500

    projects page started

--- client/components/ProjectPage.jsx
+++ client/components/ProjectPage.jsx
@@ -4,12 +4,33 @@ function ProjectPage() {
   return (
     <section className="app-screen" id="project-page-container">
       <div className="project-container">
-        <header className="project-title">E-Commerce Website - NO CAP</header>
-        <img className="project-images" alt="No-Cap" />
-        <header className="project-title">
-          Finance Management Application - FundIt
-        </header>
-        <img className="project-images" alt="FundIt" />
+        <div className="project-box">
+          <header className="project-title">E-Commerce Website - NO CAP</header>
+          <img
+            className="project-images"
+            alt="No-Cap"
+            src="public/photos/No-Cap.png"
+          />
+          <span>
+            NoCap is a simple e-commerce hat shop build by 5 developers in two
+            weeks.
+          </span>
+          <span>
+            Technologies used: Node, Express, React, Postgresql, Sequelize
+          </span>
+          <a href="https://nocap-demo.herokuapp.com/">
+            <button type="button">DEMO</button>
+          </a>
+          <a href="https://github.com/AnnaLitovskaya/No-Cap">
+            <button type="button">CODE</button>
+          </a>
+        </div>
+        <div className="project-box">
+          <header className="project-title">
+            Finance Management Application - FUNDIT
+          </header>
+          <img className="project-images" alt="FundIt" />
+        </div>
       </div>
     </section>
   );
