ca46873beb9f2a9c373d1a6cc67e9ebf0d94f22a
Anna Litovskaya <Litovskaya09@gmail.com>
2022-02-11 02:35:52 -0500

    about me complete

--- client/components/AboutPage.jsx
+++ client/components/AboutPage.jsx
@@ -4,15 +4,24 @@ function AboutPage() {
   return (
     <section className="app-screen" id="about-page-container">
       <div id="about-page-text">
-        My background is in mechanical engineering and industrial design. I have
-        been a CNC machine programmer for 7 years before making the radical
-        change of careers to web developement. I am most excited when I come
-        across a difficult problem that needs a new and innovative solution.
-        When I am not persuing my career goals or printing a widget on my 3D
-        printer, you can catch me hiking in the mountains or attempting to climb
-        a large rock.
+        <span>About Me</span>
+        <span>
+          My background is in mechanical engineering and industrial design.
+          Before I became a Full Stack Web Developer I worked in manufacturing
+          as a CNC machine programmer for 6 years.
+        </span>
+        <span>
+          I graduated Fullstack Academy software engineering immersive program
+          which focuses on full stack development using JavaScript including
+          Node.js, Express, Sequelize, PostgreSQL, React, Redux, and CS
+          fundamentals including data structures and algorithms.
+        </span>
+        <span>
+          When I'm not coding, I'm making widgets on my 3D printer, hiking,
+          reading, playing music, or eating a burrito.
+        </span>
       </div>
-      <img alt="Me" />
+      <img alt="Me" src="public/photos/Me.jpeg" />
     </section>
   );
 }
